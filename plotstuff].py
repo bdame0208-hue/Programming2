85abeaa15c7918062f21d74dcc8d017ec9c2593f
Braylon Dame <braylondame@Abigails-MacBook-Air.local>
2025-11-07 09:36:25 -0600

    changed the x and y axis

--- plotstuff].py
+++ plotstuff].py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-x=[1,3,5]
-y=[11,20,9]
+x=[1,3,5,7,9]
+y=[11,20,9,8,5]
 plt.plot(x,y)
 plt.title("Plot Example")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
-plt.savefig("Example.png")
+plt.show()
